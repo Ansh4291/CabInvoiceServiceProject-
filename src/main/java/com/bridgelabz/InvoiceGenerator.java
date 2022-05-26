1315aa84ad611737f28b9e8691637e96b6f826f3
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-27 00:09:12 +0530

    Added:Welcome message added

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cab Invoice Generator project :-) ");
+    }
+}
