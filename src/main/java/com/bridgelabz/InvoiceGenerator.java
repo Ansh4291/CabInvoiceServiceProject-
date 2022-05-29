453c7c7e3224a94f05f349a2e43795ba1fc92df2
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-29 17:33:44 +0530

    Added:Given Multiple Rides Should Return Total Fare.

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -4,8 +4,19 @@ public class InvoiceGenerator {
 
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculatefare(double distance , int time) {
-            return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+            double totalfare =  distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+            if (totalfare < MINIMUM_FARE)
+                return MINIMUM_FARE;
+            return totalfare;
+    }
+    public double calculatefare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides) {
+            totalFare += this.calculatefare(ride.distance , ride.time);
+        }
+        return totalFare;
     }
 }
